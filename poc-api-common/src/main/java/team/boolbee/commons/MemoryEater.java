dad40bdbf02f0f339e3c5d8414d2a5859b2536ba
caalpeva <caalpeva@yahoo.es>
2017-10-25 16:52:27 +0200

    Se realizan cambios sobre la clase MemoryEater.

--- poc-api-common/src/main/java/team/boolbee/commons/MemoryEater.java
+++ poc-api-common/src/main/java/team/boolbee/commons/MemoryEater.java
@@ -4,14 +4,15 @@ import java.util.Vector;
 
 public class MemoryEater {
 	
-	private static final int MB = 1048576;
+	private static final int KB = 1024;
+	private static final int MB = 1024 * KB;
 	
 	public static void main(String[] args) throws InterruptedException{
 		while (true) {
 			long freeMemory = getFreeMemory();
 			Vector<byte[]> v = new Vector<byte[]>();
 			do{
-		    	v.add(new byte[MB]);
+		    	v.add(new byte[100 * MB]);
 		    	freeMemory = getFreeMemory();
 			} while (freeMemory > 512 * MB);
 			System.out.println("Max memory consumed. Sleep for 1 seconds and continue");
@@ -21,7 +22,7 @@ public class MemoryEater {
 	
 	private static long getFreeMemory(){
 		long freeMemory = Runtime.getRuntime().freeMemory();
-    	System.out.println("Free memory: " + getMb(freeMemory));
+    	System.out.println(System.out.printf("Free memory: %.2f MB", getMb(freeMemory)));
     	return freeMemory;
 	}
 	
