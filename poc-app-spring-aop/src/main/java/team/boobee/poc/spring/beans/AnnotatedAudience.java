baf0b372a621fb7ca1a1a0391d585cff55b9c0f0
caalpeva <caalpeva@yahoo.es>
2017-11-10 17:56:39 +0100

    Se realizan modificaciones en el proyecto y se eliminan dependencias innecesarias.

--- poc-app-spring-aop/src/main/java/team/boobee/poc/spring/beans/AnnotatedAudience.java
+++ poc-app-spring-aop/src/main/java/team/boobee/poc/spring/beans/AnnotatedAudience.java
@@ -1,9 +1,9 @@
 package team.boobee.poc.spring.beans;
 
-import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
