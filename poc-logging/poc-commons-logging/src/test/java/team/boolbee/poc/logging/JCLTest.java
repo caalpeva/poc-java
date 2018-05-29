04fc5991892c6ba83e71155082922b3f9ddd6dd3
caalpeva <caalpeva@yahoo.es>
2018-05-29 16:51:51 +0200

    Se realizan algunas modificaciones.

--- poc-logging/poc-commons-logging/src/test/java/team/boolbee/poc/logging/JCLTest.java
+++ poc-logging/poc-commons-logging/src/test/java/team/boolbee/poc/logging/JCLTest.java
@@ -5,6 +5,14 @@ import junit.framework.TestCase;
 public class JCLTest extends TestCase {
 
 	public void testLogging() {
-		new LogRegistrar().register("This is a message");
+		while(true) {
+			new LogRegistrar().register("This is a message");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
