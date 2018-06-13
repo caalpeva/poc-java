5c5a733f7836039b29d2381aa7e098c5c85117b3
caalpeva <caalpeva@yahoo.es>
2018-06-13 23:29:19 +0200

    Se añade nuevo módulo al proyecto poc-logging para para demostrar el funcionamiento del marco de registro de datos java.util.logging.

---
+++ poc-logging/poc-logging-framework-jul/src/main/java/team/boolbee/poc/logging/jul/Island.java
@@ -0,0 +1,50 @@
+package team.boolbee.poc.logging.jul;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Island {
+	static {
+		Handler fh;
+		try {
+//			String path = Island.class.getClassLoader().getResource("jul-logging.properties").getFile();
+//			System.setProperty("java.util.logging.config.file", path);
+
+			fh = new FileHandler("%t/wombat.log");
+			Logger.getLogger("").addHandler(fh);
+			Logger.getLogger("team.boolbee.poc.logging.jul").setLevel(Level.FINEST);
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void execute() {
+		// public static Logger logger = Logger.getLogger("com.wombat.nose");
+		Logger logger = Logger.getLogger("team.boolbee.poc.logging.jul");
+		// private static FileHandler fh = new FileHandler("mylog.txt");
+		// logger.addHandler(fh);
+		// Request that every detail gets logged.
+		// logger.setLevel(Level.ALL);
+		// Log a simple INFO message.
+		logger.info("doing stuff");
+		try {
+			String txt = null;
+			txt.equals(null);
+		} catch (Exception ex) {
+			logger.log(Level.WARNING, "trouble sneezing", ex);
+		}
+		logger.fine("done");
+	}
+	
+	public static void main(String[] args) {
+		Island.execute();
+	}
+
+}
---
+++ poc-logging/poc-logging-framework-jul/src/test/java/team/boolbee/poc/logging/jul/Sdk14LoggingTest.java
@@ -0,0 +1,11 @@
+package team.boolbee.poc.logging.jul;
+
+
+import junit.framework.TestCase;
+
+public class Sdk14LoggingTest extends TestCase {
+
+	public void testLogging() {
+		Island.execute();
+	}
+}
