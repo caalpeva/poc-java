05607888b17137942f4c798859363747dbd329dc
caalpeva <caalpeva@yahoo.es>
2019-01-16 13:15:43 +0100

    Se añaden nuevos módulos de proyecto para comprobar el funcionamiento de servicios remotos y de webservices.

---
+++ poc-framework-spring/poc-framework-spring-09-remoting/poc-framework-spring-09-remoting-rmi/src/main/java/team/boolbee/poc/spring/remoting/model/Citation.java
@@ -0,0 +1,36 @@
+package team.boolbee.poc.spring.remoting.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Citation implements Serializable {
+   private Date date;
+   private String violationCode;
+   private String description;
+
+   public Citation() {}
+
+   public Date getDate() {
+      return date;
+   }
+
+   public void setDate(Date date) {
+      this.date = date;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public String getViolationCode() {
+      return violationCode;
+   }
+
+   public void setViolationCode(String violationCode) {
+      this.violationCode = violationCode;
+   }
+}
