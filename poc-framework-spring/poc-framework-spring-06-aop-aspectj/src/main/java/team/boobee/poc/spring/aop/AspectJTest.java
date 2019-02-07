04020c0cad92d52f3aefca285abca8b9cf678677
caalpeva <caalpeva@yahoo.es>
2019-02-07 19:25:15 +0100

    Se añaden las dependencias correctas para ejecutar AspectJ.

--- poc-framework-spring/poc-framework-spring-06-aop-aspectj/src/main/java/team/boobee/poc/spring/aop/AspectJTest.java
+++ poc-framework-spring/poc-framework-spring-06-aop-aspectj/src/main/java/team/boobee/poc/spring/aop/AspectJTest.java
@@ -8,7 +8,6 @@ import team.boolbee.poc.spring.beans.TalentCompetition;
 public class AspectJTest {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("context-aop-aspectj.xml");
 		TalentCompetition competition = (TalentCompetition) context.getBean("springIdol");
 		competition.run();
