9dc51cbb3060a45c1e89d9a4456ebeaffdf49508
caalpeva <caalpeva@yahoo.es>
2019-03-15 17:21:16 +0100

    Se crea modulo de proyecto resolviendo los problemas con el IDE.

---
+++ poc-spring-5.x/poc-spring-5.x-02-persistence-jpa/src/main/java/team/boolbee/poc/spring/model/News.java
@@ -0,0 +1,63 @@
+package team.boolbee.poc.spring.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class News {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	
+	@Column(name = "publicationDate")
+	private Date date = new Date();
+	private String detail;
+	
+	@Enumerated(EnumType.STRING)
+	private Status status = Status.ACTIVE;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	
+	@Override
+	public String toString() {
+		return "News [id=" + id + ", title=" + title + ", date=" + date + ", detail=" + detail + ", status=" + status
+				+ "]";
+	}
+}
---
+++ poc-spring-5.x/poc-spring-5.x-02-persistence-jpa/src/main/java/team/boolbee/poc/spring/model/Status.java
@@ -0,0 +1,5 @@
+package team.boolbee.poc.spring.model;
+
+public enum Status {
+	ACTIVE, INACTIVE
+}
