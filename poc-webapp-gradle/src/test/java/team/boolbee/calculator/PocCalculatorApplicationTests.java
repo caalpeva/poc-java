706179b714059ff39708f70343807685b7a2dd66
caalpeva <caalpeva@yahoo.es>
2022-04-13 10:45:33 +0200

    Modify app code

---
+++ poc-webapp-gradle/src/test/java/team/boolbee/calculator/PocCalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package team.boolbee.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+* SpringBoot context test.
+*/
+@SpringBootTest
+public class PocCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
