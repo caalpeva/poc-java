67a2e925a129adf85fc2c7ac7e861e6efedf851f
caalpeva <boolbee01@gmail.com>
2023-04-22 17:45:44 +0200

    Add proof of concept about cadence workflows

---
+++ poc-cadence/cadence/activities/activities-mobile-numeration/src/main/java/team/kalpeva/poc/cadence/mobile/router/Main.java
@@ -0,0 +1,8 @@
+package team.kalpeva.poc.cadence.mobile.router;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ poc-cadence/uservices/poc-mobile-router/src/main/java/team/kalpeva/poc/cadence/mobile/router/starter/Main.java
@@ -0,0 +1,8 @@
+package team.kalpeva.poc.cadence.mobile.router.starter;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
